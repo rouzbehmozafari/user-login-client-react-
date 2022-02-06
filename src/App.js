d6c92f7f52a684371e4b6685928fd982e696eeb7
“rouzbehmozafari” <rouzbeh.mozafari@yahoo.com>
2022-02-06 22:08:32 +0100

    basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [email,setEmail] = useState('')
+  const [pass,setPass] = useState('')
+  const [firstName,setFirstName] = useState('')
+  const [lastName,setLastName] = useState('')
+  const [newEmail,setNewEmail] = useState('')
+  const [newPass,setNewPass] = useState('')
+
+  const urlLogin = 'http://localhost:8080/login'
+  const urlRegister = 'http://localhost:8080/register'
+
+  const login = (e)=> {
+    e.preventDefault()
+    const userInfo = {email,pass}
+    axios.post(urlLogin,userInfo).then((respond)=>{console.log(respond.data)})
+  }
+  const signUp = (e ) => {
+    e.preventDefault()
+    const newUser = {firstName,lastName,newEmail,newPass}
+    axios.post(urlRegister,newUser).then((respond)=>{console.log(respond.data)})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login</h1>
+      <form action="">
+        <input type="email" onClick={e => {setEmail(e.target.value)}} />
+        <input type="password" onClick={e => {setPass(e.target.value)}}/>
+        <input type="submit" onClick={login} value='login' />
+      </form>
+      <h1>Sign up</h1>
+      <form action="">
+        <input type="text" placeholder='firstName' defaultValue={firstName}  onChange={e => setFirstName(e.target.value)} />
+        <input type="text" placeholder='lastName' defaultValue={lastName}  onChange={e => setLastName(e.target.value)} />
+        <input type="email" placeholder='Email' defaultValue={newEmail} onChange={e => setNewEmail(e.target.value)} />
+        <input type="password" placeholder='Password' defaultValue={newPass}  onChange={e => setNewPass(e.target.value)}/>
+        <input type="submit" onClick={signUp} value='Sign up' />
+      </form>
     </div>
   );
 }
